1df1189994d2aab792df7699b10be3e5e35ce13e
Heuller Soares Vilela Silva <heullergit@gmail.com>
2019-10-03 15:26:26 -0300

    pequenos ajustes no back, arrumar o front

--- app.js
+++ app.js
@@ -8,63 +8,111 @@ $(document).ready(function(){
         [' ', ' ', ' ']
     ];
 
-    function acabouJogo(){
+    function acabouJogo(novaGrid){
         //Verifica se o jogo acabou
         //Verificar horizontal, vertical e diagonal
 
         //Horizontal
         for (var i = 0; i < 3; i++) {
-            if(grid[i][0] !== ' ' && grid[i][0] === grid[i][1] && grid[i][0] == grid[i][2]){
-                return grid[i][0]
+            if(novaGrid[i][0] !== ' ' && novaGrid[i][0] === novaGrid[i][1] && novaGrid[i][0] == novaGrid[i][2]){
+                return novaGrid[i][0]
             }
         }
 
         //Vertical
         for (var i = 0; i < 3; i++) {
-            if(grid[0][i] !== ' ' && grid[0][i] === grid[1][i] && grid[0][i] == grid[2][i]){
-                return grid[0][i]
+            if(novaGrid[0][i] !== ' ' && novaGrid[0][i] === novaGrid[1][i] && novaGrid[0][i] == novaGrid[2][i]){
+                return novaGrid[0][i]
             }
         }
 
         //Diagonais
         //Topo esquerda
-        if(grid[0][0] !== ' ' && grid[0][0] === grid[1][1] && grid[0][0] == grid[2][2]){
-            return grid[0][0]
+        if(novaGrid[0][0] !== ' ' && novaGrid[0][0] === novaGrid[1][1] && novaGrid[0][0] == novaGrid[2][2]){
+            return novaGrid[0][0]
         }
 
         //Topo direita
-        if(grid[0][2] !== ' ' && grid[0][2] === grid[1][1] && grid[0][2] == grid[2][0]){
-            return grid[0][2]
+        if(novaGrid[0][2] !== ' ' && novaGrid[0][2] === novaGrid[1][1] && novaGrid[0][2] == novaGrid[2][0]){
+            return novaGrid[0][2]
         }
 
 
         for(var i = 0; i < 3; i++){
             for(var j = 0; j < 3; j++){
-                if(grid[i][j] === ' '){ //Existe posição livre e ainda não acabou o jogo
+                if(novaGrid[i][j] === ' '){ //Existe posição livre e ainda não acabou o jogo
                     return false;
                 }
             }
         }
 
         //Não existe posição livre e não há vencedores, jogo acabou empatado
-        alert('Empate')
         return null
     }
 
-    function minmax(novaGrid, profundidade, jogdor){
+    function minmax(novaGrid, profundidade, jogador){
         //Ao chamar a função minmax verifica a situação do jogo para determinar como proceder
         const estado_jogo = acabouJogo(novaGrid);
 
-        if(estado_jogo === false){
+        if(estado_jogo === false){ //Jogo em aberto, ainda há possibilidade de jogadas (novos nós)
+            const valores = [];
+
+            for (var i = 0; i < 3; i++) {
+                for (var j = 0; j < 3; j++) {
+                    const grid_copia = _.cloneDeep(novaGrid);
+                    if(grid_copia[i][j] === ' ') {
+                        grid_copia[i][j] = jogador;
+                        const valor = minmax(grid_copia, profundidade + 1, (jogador === JOGADOR_TOKEN)? COMPUTADOR_TOKEN : JOGADOR_TOKEN); //Alterna o valor do jogador
+                        valores.push({
+                            custo: valor,
+                            jogada: {
+                                i: i,
+                                j: j
+                            }
+                        });
+                    }
+                }
+            }
+
+            if(jogador === COMPUTADOR_TOKEN){ //Retorna o max quando é a vez do computador
+                const max = _.maxBy(valores, (v) =>{
+                    return v.custo;
+                });
+                if(profundidade === 0){
+                    return max.jogada;
+                }
+                else{
+                    return max.custo;
+                }
+            }
+            else{ //Retorna min quando é a vez do jogador
+
+                const min = _.minBy(valores, (v) =>{
+                    return v.custo;
+                });
+
+                if(profundidade === 0){
+                    return min.jogada;
+                }
+                else{
+                    return min.custo;
+                }
+
+
+            }
+
+
             
         } 
-        else if(estado_jogo === null){
-
+        else if(estado_jogo === null){ //Jogo finalizado, empatado
+            return 0;
         }
-        else if(estado_jogo === JOGADOR_TOKEN){
-
+        else if(estado_jogo === JOGADOR_TOKEN){ //Jogo finalizado, jogador venceu
+            return profundidade - 10; //Mínimo para o jogador
         }
-        else if(estado_jogo === COMPUTADOR_TOKEN)
+        else if(estado_jogo === COMPUTADOR_TOKEN){ //Jogo finalizado, computador venceu
+            return 10 - profundidade; //Máximo para o computador
+        }
 
     }
 
@@ -79,7 +127,7 @@ $(document).ready(function(){
         const j = $this.data('j');
         grid[i][j] = JOGADOR_TOKEN;
 
-        if(acabouJogo()){
+        if(acabouJogo(grid)){
             alert('Você venceu')
             return;
         }
@@ -90,11 +138,9 @@ $(document).ready(function(){
             $('.col[data-i=' + movimento.i + '][data-j=' + movimento.j + ']').html(COMPUTADOR_TOKEN);
         }
 
-        if(acabouJogo()){
+        if(acabouJogo(grid)){
             alert('Você perdeu')
         }
-
-        console.log(grid)
     });
 
     $('#reiniciar').click(function(){
