e16e3302b082d3ed75f7f9e3764f175ad20af2ca
Heuller Soares Vilela Silva <heullergit@gmail.com>
2019-10-06 18:12:35 -0300

    remocao funcao sleep

--- app.js
+++ app.js
@@ -10,15 +10,6 @@ $(document).ready(function(){
         [' ', ' ', ' ']
     ];
 
-    function sleep(milliseconds) {
-        var start = new Date().getTime();
-        for (var i = 0; i < 1e7; i++) {
-          if ((new Date().getTime() - start) > milliseconds){
-            break;
-          }
-        }
-    }
-
     function acabouJogo(novaGrid){
         //Verifica se o jogo acabou
         //Verificar horizontal, vertical e diagonal
